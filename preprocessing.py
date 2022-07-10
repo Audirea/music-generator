0d6106de0185546e14756e231d31727ec6d6cd74
Ayesh Vininda <ayeshgk@gmail.com>
2022-07-10 20:00:37 +0530

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,19 @@
-import os 
+import os
 import music21 as m21
 
 # globals
 DATASET_PATH = "dataset/deutschl/test"
-ACCEPTABLE_DURATIONS=[
-    0.25 ,#sixteenth note
-    0.5 ,#eighth note
-    0.75 ,
-    1.0 ,#quarter note
-    1.5 ,
-    2.0 ,#half note
+SAVE_PATH = "preprocessed/deutschl/test"
+
+ACCEPTABLE_DURATIONS = [
+    0.25,  # sixteenth note
+    0.5,  # eighth note
+    0.75,
+    1.0,  # quarter note
+    1.5,
+    2.0,  # half note
     3,
-    4.0 ,#whole note
+    4.0,  # whole note
 ]
 
 
@@ -26,7 +28,8 @@ def load_songs(path):
             songs.append(song)
     return songs
 
-def is_acceptable_duration(song,durations):
+
+def is_acceptable_duration(song, durations):
     """
     Checks if the duration is acceptable.
     """
@@ -35,6 +38,7 @@ def is_acceptable_duration(song,durations):
             return False
 
     return True
+
 
 def transpose_song(song):
     """
@@ -47,29 +51,66 @@ def transpose_song(song):
     key_signature = measures[0][4]
 
     if not isinstance(key_signature, m21.key.Key):
-        key_signature =song.analyze('key')
+        key_signature = song.analyze('key')
 
     # get intervels for tansposition
     if key_signature.mode == "major":
-        interval = m21.interval.Interval(key_signature.tonic,m21.pitch.Pitch("C"))
+        interval = m21.interval.Interval(
+            key_signature.tonic, m21.pitch.Pitch("C"))
     elif key_signature.mode == "minor":
-        interval = m21.interval.Interval(key_signature.tonic,m21.pitch.Pitch("A"))
-    
-    
+        interval = m21.interval.Interval(
+            key_signature.tonic, m21.pitch.Pitch("A"))
+
     # transpose by intervel
     return song.transpose(interval)
 
-def encode_song(song):
+
+def encode_song(song, time_step=0.25):
     """
     Encodes the song.
-    """
-    pass
+        This gives time series representation of the song.
 
-def save_song(song,path):
+    Convert score into time series.It is representing quater lengths of notes.
+    The symblos are:
+        r: rest
+        '_': notes/rests
+        integer: for midi notes
+
+
+    parameters are: 
+    song: music21.stream
+    time_step : duraion of each time step in quater length
+
+
+    return : string (encoded song as time series string)
+    """
+
+    encoded_songs = []
+
+    for event in song.flat.notesAndRests:
+        # for notes
+        if isinstance(event, m21.note.Note):
+            symbol = event.pitch.midi
+
+        # for rests
+        elif isinstance(event, m21.note.Rest):
+            symbol = "r"
+
+        for step in range(int(event.duration.quarterLength/time_step)):
+            if(step == 0):
+                encoded_songs.append(symbol)
+            else:
+                encoded_songs.append("_")
+
+    return " ".join(map(str, encoded_songs))
+
+
+def save_song(song, path):
     """
     Saves the song to the given path.
     """
-    song.write("midi",path)
+    with open(path, "w") as f:
+        f.write(song)
 
 
 def dataset_preprocessing():
@@ -78,29 +119,34 @@ def dataset_preprocessing():
     """
     # load dataset
     songs = load_songs(DATASET_PATH)
-    
-    
-    for song in songs:
+
+    for i, song in enumerate(songs):
         # filter using acceptance criteria
-        if not is_acceptable_duration(song,ACCEPTABLE_DURATIONS):
+        if not is_acceptable_duration(song, ACCEPTABLE_DURATIONS):
             continue
-        
+
         # transpose songs
         song = transpose_song(song)
 
         # endcode song
+        encoded_song = encode_song(song)
 
         # write to file
 
-    
+        save_song(encoded_song, SAVE_PATH + "/" + str(i) + ".txt")
+
 
 if __name__ == "__main__":
-    
+
     # test
 
-    songs = load_songs(DATASET_PATH)
-    print(len(songs))
+    # songs = load_songs(DATASET_PATH)
+    # print(len(songs))
 
-    
-    transposed_song = transpose_song(songs[0])
-    print(transposed_song.analyze('key'))
+    # transposed_song = transpose_song(songs[0])
+    # print(transposed_song.analyze('key'))
+    # songs[0].show()
+
+    if not os.path.exists(SAVE_PATH):
+        os.makedirs(SAVE_PATH)
+    dataset_preprocessing()
